ecfe79ac29d108043228892e1df03d3a93f27f49
mahak shivhare <shivharemahak272@gmail.com>
2026-03-18 16:52:02 +0530

    Add files via upload

---
+++ print_ascii_code.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class print_ascii_code {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        
+        char ch=sc.next().charAt(0);
+
+        
+    
+            System.out.println("my ascii value of character is: "+ch);
+        }
+        }
